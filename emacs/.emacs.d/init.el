5cf6dea3b9fca403d0af5abe9d197d1678635801
Samuel Hodgkins <samuel.hodgkins@sky.com>
2016-01-19 03:21:22 +0000

    Updated .gitignore and moved package archives out of the Customize section. Also shuffled around and cleaned up the theming.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,29 +1,22 @@
 (setq vc-follow-symlinks nil)
 (add-hook 'after-init-hook 'packages-init-hook)
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                         ("marmalade" . "https://marmalade-repo.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
 (defun packages-init-hook ()
+  (load-theme 'monokai t)
   (setq ergoemacs-theme "custom")
   (setq ergoemacs-keyboard-layout "gb")
   (require 'ergoemacs-mode)
   (load-file "~/.emacs.d/ergoemacs-custom-theme.el")
-  (ergoemacs-mode 1)
-  (load-theme 'monokai t)
-  )
+  (ergoemacs-mode 1))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (monokai)))
- '(custom-safe-themes
-   (quote
-    ("0fb6369323495c40b31820ec59167ac4c40773c3b952c264dd8651a3b704f6b5" default)))
  '(global-linum-mode t)
- '(package-archives
-   (quote
-    (("gnu" . "http://elpa.gnu.org/packages/")
-     ("melpa" . "https://melpa.org/packages/")
-     ("marmalade" . "https://marmalade-repo.org/packages/"))))
  '(scroll-error-top-bottom nil)
  '(set-mark-command-repeat-pop nil)
  '(shift-select-mode t))
