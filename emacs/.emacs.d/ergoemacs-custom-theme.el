ab32d5962c0255ef39ab78b9b3981cb8862ecef8
Samuel Hodgkins <samuel.hodgkins@sky.com>
2016-01-21 20:31:13 +0000

    First reduction of custom ergoemacs theme. Also includes transient GTK/htop changes.

--- emacs/.emacs.d/ergoemacs-custom-theme.el
+++ emacs/.emacs.d/ergoemacs-custom-theme.el
@@ -48,26 +48,13 @@
                  menu-bar-edit
                  menu-bar-file
                  )
-  :optional-off '(guru
-                  alt-backspace-is-undo
-                  search-reg
-                  no-backspace
-                  ergoemacs-banish-shift
+  :optional-off '(alt-backspace-is-undo
                   move-and-transpose-lines
                   move-sexp
-                  helm-switch-sources
-                  helm-files-up
-                  ido-prev-next-instead-of-left-right
-                  join-line
-                  isearch-arrows)
+                  ido-prev-next-instead-of-left-right)
   :options-menu '(("Menu/Apps Key" (apps apps-apps apps-punctuation apps-toggle))
                   ("Function Keys" (fn-keys f2-edit))
-                  ("Helm Options" (helm-switch-sources helm-files-up))
-                  ("Remaps" (ido-remaps helm-remaps multiple-cursors-remaps icy-reclaim))
-                  ("Extreme ErgoEmacs" (guru no-backspace ergoemacs-banish-shift))
                   ("Standard Keys" (standard-fixed fixed-bold-italic quit move-and-transpose-lines alt-backspace-is-undo))
                   ("Keys during Key Sequence" (f2-edit apps-swap backspace-del-seq))
                   ("Disputed Keys" (ido-prev-next-instead-of-left-right move-sexp))
-                  ("Extra Functionality" (join-line isearch-arrows))
-                  ("Packages" (avy multiple-cursors expand-region))
-                  ("Ergoemacs global menus" (menu-bar-file menu-bar-edit menu-bar-search menu-bar-view menu-bar-languages menu-bar-help))))
+                  ("Menus" (menu-bar-file menu-bar-edit menu-bar-search menu-bar-view menu-bar-languages menu-bar-help))))
