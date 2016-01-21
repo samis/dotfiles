867e067e13d96175db6312a9f93f2bff26aa2147
Samuel Hodgkins <samuel.hodgkins@sky.com>
2016-01-21 23:50:26 +0000

    Finished version of ergoemacs custom theme for now.

--- emacs/.emacs.d/ergoemacs-custom-theme.el
+++ emacs/.emacs.d/ergoemacs-custom-theme.el
@@ -1,27 +1,15 @@
 (ergoemacs-theme custom ()
   "My custom Ergoemacs theme mostly based around the basic CUA/Windows shortcuts"
-  :components '(copy
-                dired-tab
-                dired-to-wdired
-                execute
-                fixed-newline
-                help
-                kill-line
-                move-bracket
-                switch
+  :components '(move-bracket
+		standard-fixed
                 ergoemacs-remaps
                 standard-vars)
   :optional-on '(tab-indents-region
                  fixed-bold-italic
-                 standard-fixed
                  ;; Reverse menu-bar order
                  menu-bar-help
                  menu-bar-languages
                  menu-bar-view
                  menu-bar-search
                  menu-bar-edit
-                 menu-bar-file
-                 )
-  :optional-off '(move-sexp)
-  :options-menu '(("Standard Keys" (standard-fixed fixed-bold-italic))
-                  ("Menus" (menu-bar-file menu-bar-edit menu-bar-search menu-bar-view menu-bar-languages menu-bar-help))))
+                 menu-bar-file))
