eb66bcfbebe6b0becfc9ee139c71c19a95abafc9
Samuel Hodgkins <samuel.hodgkins@sky.com>
2016-01-21 23:55:20 +0000

    Formatting.

--- emacs/.emacs.d/ergoemacs-custom-theme.el
+++ emacs/.emacs.d/ergoemacs-custom-theme.el
@@ -1,15 +1,17 @@
+;;; ergoemacs-custom-theme.el --- My custom theme for ergoemacs-mode.
+
+;;; Commentary:
+;; 
+
+;;; Code:
+
 (ergoemacs-theme custom ()
   "My custom Ergoemacs theme mostly based around the basic CUA/Windows shortcuts"
-  :components '(move-bracket
-		standard-fixed
-                ergoemacs-remaps
-                standard-vars)
-  :optional-on '(tab-indents-region
-                 fixed-bold-italic
-                 ;; Reverse menu-bar order
-                 menu-bar-help
-                 menu-bar-languages
-                 menu-bar-view
-                 menu-bar-search
-                 menu-bar-edit
-                 menu-bar-file))
+  :components '(move-bracket standard-fixed ergoemacs-remaps standard-vars)
+  ; For menu bars, the order is reversed.
+  :optional-on '(tab-indents-region fixed-bold-italic menu-bar-help menu-bar-languages menu-bar-view menu-bar-search
+		 menu-bar-edit menu-bar-file))
+
+(provide 'ergoemacs-custom-theme)
+
+;;; ergoemacs-custom-theme.el ends here
