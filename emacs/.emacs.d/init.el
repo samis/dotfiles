dff1c7001c1b929a89a53e4686a99ebe7e77cbe8
Samuel Hodgkins <samuel.hodgkins@sky.com>
2016-01-22 01:23:02 +0000

    Installed el-get as a package manager.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,3 +1,14 @@
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
+(el-get 'sync)
 (setq vc-follow-symlinks nil)
 (tool-bar-mode -1)
 (add-hook 'after-init-hook 'packages-init-hook)
