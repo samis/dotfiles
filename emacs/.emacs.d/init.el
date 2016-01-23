e4d958da5d7fceca53c56035df992cd15b04e1f5
Samuel Hodgkins <samuel.hodgkins@sky.com>
2016-01-23 03:38:37 +0000

    Move monokai theme enabling into own file and remove now-unused hook.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -8,19 +8,16 @@
     (eval-print-last-sexp)))
 
 (add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
-(setq my-packages '(el-get markdown-mode ergoemacs-mode monokai-theme magit undo-tree unicode-fonts))
+(setq my-packages '(el-get monokai-theme markdown-mode ergoemacs-mode magit undo-tree unicode-fonts))
 (setq el-get-packages
       (append my-packages
               (mapcar #'el-get-source-name el-get-sources)))
 (el-get 'sync el-get-packages)
 (setq vc-follow-symlinks nil)
 (tool-bar-mode -1)
-(add-hook 'after-init-hook 'packages-init-hook)
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
                          ("marmalade" . "https://marmalade-repo.org/packages/")
                          ("melpa" . "https://melpa.org/packages/")))
-(defun packages-init-hook ()
-  (load-theme 'monokai t))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
