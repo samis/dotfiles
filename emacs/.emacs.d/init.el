e11169047c5ddf7159d4ea1b90036966abdef182
Samuel Hodgkins <samuel.hodgkins@sky.com>
2016-01-23 22:42:10 +0000

    Removed all use of customize from the init file.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -16,16 +16,7 @@
 (el-get 'sync el-get-packages)
 (setq vc-follow-symlinks nil)
 (tool-bar-mode -1)
+(global-linum-mode t)
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
                          ("marmalade" . "https://marmalade-repo.org/packages/")
                          ("melpa" . "https://melpa.org/packages/")))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(global-linum-mode t)
- '(scroll-error-top-bottom nil)
- '(set-mark-command-repeat-pop nil)
- '(shift-select-mode t))
