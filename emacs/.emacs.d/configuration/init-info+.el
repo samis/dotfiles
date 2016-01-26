b69e084b7b8b40f4a0e554363f5462cbe627ab1f
Samuel Hodgkins <samuel.hodgkins@sky.com>
2016-01-26 01:00:18 +0000

    Fix colours.

--- emacs/.emacs.d/configuration/init-info+.el
+++ emacs/.emacs.d/configuration/init-info+.el
@@ -2,6 +2,3 @@
 (setq Info-fontify-quotations-flag t)
 (setq Info-fontify-angle-bracketed-flag t)
 (setq Info-fontify-single-quote-flag’ t)
-(setq special-display-buffer-names
-      (cons '("*info*" (background-color . "LightSteelBlue"))
-	    special-display-buffer-names))
