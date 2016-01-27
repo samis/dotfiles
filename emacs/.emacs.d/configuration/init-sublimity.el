8fa485a8eab261a8643bf760560d78ab2b041bfe
Samuel Hodgkins <samuel.hodgkins@sky.com>
2016-01-27 22:48:20 +0000

    Tweak my emacs configuration for sublimity a bit.

--- emacs/.emacs.d/configuration/init-sublimity.el
+++ emacs/.emacs.d/configuration/init-sublimity.el
@@ -1,7 +1,7 @@
 (require 'sublimity)
 (require 'sublimity-scroll)
 (require 'sublimity-map)
-(sublimity-mode 1)
 (sublimity-map-set-delay nil)
 (setq sublimity-scroll-weight 5
       sublimity-scroll-drift-length 10)
+(sublimity-mode 1)
