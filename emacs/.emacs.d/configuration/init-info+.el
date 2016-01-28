1828bec6f5ac5a4384fb319594d8a3ffa3eb01b2
Samuel Hodgkins <samuel.hodgkins@sky.com>
2016-01-28 22:46:59 +0000

    Unbreak scrollwheel in info mode.

--- emacs/.emacs.d/configuration/init-info+.el
+++ emacs/.emacs.d/configuration/init-info+.el
@@ -1,4 +1,7 @@
 (eval-after-load "info" '(require 'info+))
+(eval-after-load "info+" '(progn
+			    (define-key Info-mode-map [mouse-4]         'mwheel-scroll)
+			    (define-key Info-mode-map [mouse-5]         'mwheel-scroll)))
 (setq Info-fontify-quotations-flag t)
 (setq Info-fontify-angle-bracketed-flag t)
 (setq Info-fontify-single-quote-flag’ t)
