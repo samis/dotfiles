df1dedfa4398542cb4e8e4cccfce7c58bbb4136e
Samuel Hodgkins <samuel.hodgkins@sky.com>
2016-02-18 03:09:14 +0000

    Tweak org-mode configuration for TODO states

--- emacs/.emacs.d/configuration/init-org-mode.el
+++ emacs/.emacs.d/configuration/init-org-mode.el
@@ -2,3 +2,5 @@
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
 (setq org-log-done t)
+(setq org-todo-keywords
+      '((sequence "TODO" "INPROGRESS" "WAITING" "|" "DONE" "CANCELLED")))
