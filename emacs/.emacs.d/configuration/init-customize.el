d7bd6fcc3148afebc8f7b434824ab9d250d843a3
Samuel Hodgkins <samuel.hodgkins@sky.com>
2016-02-25 01:53:30 +0000

    Tweaked formattinng of global TODO list

--- emacs/.emacs.d/configuration/init-customize.el
+++ emacs/.emacs.d/configuration/init-customize.el
@@ -4,22 +4,28 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(csv-separators (quote ("," ";")))
+ '(org-agenda-prefix-format
+   (quote
+    ((agenda . " %i %-12:c%?-12t% s")
+     (timeline . "  % s")
+     (todo . " %i %-6:c%l")
+     (tags . " %i %-12:c")
+     (search . " %i %-12:c"))))
  '(safe-local-variable-values
    (quote
     ((eval ignore-errors "Write-contents-functions is a buffer-local alternative to before-save-hook"
-	   (add-hook
-	    (quote write-contents-functions)
-	    (lambda nil
-	      (delete-trailing-whitespace)
-	      nil))
-	   (require
-	    (quote whitespace))
-	   "Sometimes the mode needs to be toggled off and on."
-	   (whitespace-mode 0)
-	   (whitespace-mode 1))
+           (add-hook
+            (quote write-contents-functions)
+            (lambda nil
+              (delete-trailing-whitespace)
+              nil))
+           (require
+            (quote whitespace))
+           "Sometimes the mode needs to be toggled off and on."
+           (whitespace-mode 0)
+           (whitespace-mode 1))
      (whitespace-line-column . 80)
-     (whitespace-style face tabs trailing lines-tail))))
- )
+     (whitespace-style face tabs trailing lines-tail)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
