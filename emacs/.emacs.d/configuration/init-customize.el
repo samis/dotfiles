dfb7fc0623a114086e70ebc0e86f2f28ded2ac3f
Samuel Hodgkins <samuel.hodgkins@sky.com>
2016-02-29 01:52:40 +0000

    Remove now-unused settings from Customize

--- emacs/.emacs.d/configuration/init-customize.el
+++ emacs/.emacs.d/configuration/init-customize.el
@@ -3,15 +3,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(company-quickhelp-delay 0)
- '(csv-separators (quote ("," ";")))
- '(org-agenda-prefix-format
-   (quote
-    ((agenda . " %i %-12:c%?-12t% s")
-     (timeline . "  % s")
-     (todo . " %i %-6:c%l")
-     (tags . " %i %-12:c")
-     (search . " %i %-12:c"))))
  '(safe-local-variable-values
    (quote
     ((eval ignore-errors "Write-contents-functions is a buffer-local alternative to before-save-hook"
