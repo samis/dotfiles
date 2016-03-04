d7c6750ead2307af9aeca98b84cd4d22079ad359
Samuel Hodgkins <samuel.hodgkins@sky.com>
2016-03-04 22:40:37 +0000

    Tweak ido overrides to use it for refiling in Org

--- emacs/.emacs.d/configuration/init-customize.el
+++ emacs/.emacs.d/configuration/init-customize.el
@@ -3,6 +3,22 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ido-ubiquitous-auto-update-overrides t)
+ '(ido-ubiquitous-command-overrides
+   (quote
+    ((disable exact "execute-extended-command")
+     (enable prefix "wl-")
+     (enable-old prefix "Info-")
+     (enable exact "webjump")
+     (enable regexp "\\`\\(find\\|load\\|locate\\)-library\\'")
+     (enable prefix "org-")
+     (disable prefix "tmm-")
+     (enable regexp "\\`\\(load\\|enable\\|disable\\|describe\\|custom-theme-visit\\)-theme\\'")
+     (enable-old prefix "bbdb-")
+     (enable-old exact "where-is")
+     (disable exact "todo-add-category")
+     (enable exact "find-tag")
+     (enable prefix "etags-select-"))))
  '(safe-local-variable-values
    (quote
     ((eval ignore-errors "Write-contents-functions is a buffer-local alternative to before-save-hook"
