66f7194b7b6187ca6ca3e154675fe28464792fb5
Samuel Hodgkins <samuel.hodgkins@sky.com>
2016-03-06 01:43:11 +0000

    Add aggressive-indent-mode as a package

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,3 +1,3 @@
 (load-file "~/.emacs.d/lisp/init-loader.el")
-(setq my-packages '(el-get monokai-theme markdown-mode magit undo-tree unicode-fonts spaceline info+ linkd yaml-mode geiser matrix-client sr-speedbar org-mode csv-mode better-defaults pos-tip company-mode company-quickhelp smex ido-ubiquitous diminish))
+(setq my-packages '(el-get monokai-theme markdown-mode magit undo-tree unicode-fonts spaceline info+ linkd yaml-mode geiser matrix-client sr-speedbar org-mode csv-mode better-defaults pos-tip company-mode company-quickhelp smex ido-ubiquitous aggressive-indent-mode diminish))
 (init-loader-load "~/.emacs.d/init")
