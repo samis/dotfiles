56ade5f2d7215e8b0fdfe4ed2f093ecd8d87bfaa
Samuel Hodgkins <samuel.hodgkins@sky.com>
2016-03-08 00:29:15 +0000

    Move some ido-related settings outside of custom

--- emacs/.emacs.d/configuration/init-customize.el
+++ emacs/.emacs.d/configuration/init-customize.el
@@ -3,7 +3,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ido-ubiquitous-auto-update-overrides t)
  '(ido-ubiquitous-command-overrides
    (quote
     ((disable exact "execute-extended-command")
