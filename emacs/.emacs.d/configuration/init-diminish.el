cc9f8c9cfffa3af13f16ef440b2ab04d9bca5996
Samuel Hodgkins <samuel.hodgkins@sky.com>
2016-03-09 21:35:10 +0000

    Hide the 'Server' text in the modeline

--- emacs/.emacs.d/configuration/init-diminish.el
+++ emacs/.emacs.d/configuration/init-diminish.el
@@ -4,3 +4,9 @@
 (diminish 'visual-line-mode)
 (diminish 'undo-tree-mode)
 (diminish 'auto-revert-mode)
+(with-eval-after-load 'server
+  (setq minor-mode-alist
+        (delete '(server-buffer-clients " Server") minor-mode-alist)))
+
+(eval-after-load "server"
+  (diminish 'server-mode))
