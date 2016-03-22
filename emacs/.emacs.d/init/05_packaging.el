1ea9b78c887965211b347cedc43cb4032f9b9d90
Samuel Hodgkins <samuel.hodgkins@sky.com>
2016-03-22 00:06:38 +0000

    Move most package code to Org

--- emacs/.emacs.d/init/05_packaging.el
+++ emacs/.emacs.d/init/05_packaging.el
@@ -1,10 +1,1 @@
-(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
-                         ("marmalade" . "https://marmalade-repo.org/packages/")
-                         ("melpa" . "https://melpa.org/packages/")))
-(setq el-get-user-package-directory "~/.emacs.d/configuration")
-(ensure-el-get-is-present)
-(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
-(setq el-get-packages
-      (append my-packages
-              (mapcar #'el-get-source-name el-get-sources)))
 (el-get 'sync el-get-packages)
