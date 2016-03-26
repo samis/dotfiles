a8bf81a9ca679d63cdeb14a1366c7f9d7a2f8982
Samuel Hodgkins <samuel.hodgkins@sky.com>
2016-03-26 01:33:06 +0000

    Update diminish configuration

--- emacs/.emacs.d/configuration/init-diminish.el
+++ emacs/.emacs.d/configuration/init-diminish.el
@@ -2,7 +2,6 @@
 (eval-after-load "org-indent"
   '(diminish 'org-indent-mode))
 (diminish 'visual-line-mode)
-(diminish 'undo-tree-mode)
 (diminish 'auto-revert-mode)
 (with-eval-after-load 'server
   (setq minor-mode-alist
